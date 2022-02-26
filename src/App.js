7278ce4e6eae5bf9764c28885b3daff50cefda0a
Aron <aronviniciusnascimento@gmail.com>
2022-02-25 21:39:21 -0300

    feat: create images

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
-
-  const[movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista total
       let list = await Tmdb.getHomeList();
+      setMovieList(list)
       console.log(list);
     };
     loadAll();
   }, []);
 
-  return(
-    <div>
-      Ola Mundo
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+       <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
     </div>
   );
 };
